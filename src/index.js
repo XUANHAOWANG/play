6e9caf7502bb549ba05223f98873c34a9b03bfd1
XUANHAO <438164432@QQ.COM>
2021-03-29 20:00:08 +1300

    5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
-function firstFunction(){
-    console.log("Hello from firstFunction");
-   }
-   
-   function secondFunction(){
+function secondFunction(){
     firstFunction();
-    console.log("The end from secondFunction");
+    console.log('hahah')
+}
+
+secondFunction();
+
+   function firstFunction(){
+       console.log('zhanima')
    }
-   secondFunction();
+
+console.log('babalaila')
+   ///////////
+   let 
+  
+  myNumber=23
+   myNumber=myNumber+1
+    newNumber=myNumber
+   console.log(newNumber) 
+   ////////
+
+let myString = '1234'//an address example:123
+ console.log(myString)
+myString = myString + '5'//1234
+console.log(myString)
+myString = myString+'6'//12345
+console.log(myString)
+myString = myString+'7'//123456    each declare has total different address
+console.log(myString)
+
+
+let myArray =[]
+myArray.push("first")
+myArray.push("second")
+myArray.push("third")
+myArray.push("fourth")
+console.log(myArray)
+myArray.pop()
+console.log(myArray)
+myArray.shift()
+console.log(myArray)
+myArray.unshift("nibababa")
+console.log(myArray)
+//////////
+
+let sum = 0
+sum = 1 + 2 + 3 + 4 + 5
+const numbers = [0
+ ]
+numbers.push(1)
+numbers.push(2)
+numbers.push(3)
+numbers.push(4)
+numbers.push(5)
+console.log(numbers)
