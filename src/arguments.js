5228c7faa6732ae5a46cab7954f99827dbb9aed3
XUANHAO <438164432@QQ.COM>
2021-06-13 15:45:36 +1200

    1

--- src/arguments.js
+++
@@ -1,9 +0,0 @@
-//在调用函数时 会传递两个隐藏参数 this和实参对象 arguments
-function fun(){
-    console.log( arguments[2] );//arguments是一个类数组对象 可以通过索引来操作数据 
-    //也可以获取长度
-    console.log(arguments.length);//arguments.length是可以用来获取实参长度的
-
-    console.log(arguments.callee);//对应当前执行的函数
-};
-fun(1,2,3);
